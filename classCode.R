cebe4d5b53a3cba37bd5928adb9ffeeb6feb9e27
Jay_Bektasevic <jbektasevic@bellarmine.edu>
2018-01-25 18:58:31 -0500

    added class notes file

---
+++ classCode.R
@@ -0,0 +1,30 @@
+ap <- AirPassengers
+diff(ap), # lag = 2
+diff(ap, lag = 2)
+
+ap_diff <- as.numeric()
+
+for (i in length(ap)){
+    ap_diff[i-1] <- ap[i] - ap[(i-1)]
+}
+ap_diff == diff(ap)
+
+
+# create an acf()   function manually
+
+acf_ap <- acf(ap)
+
+cent <- ap - mean(ap)
+n <- length(cent)
+ACF <- 0
+z <- sum(cent * cent)/n
+
+ACF[1] <- z/z
+
+for (i in 1:24) {
+    lag <- cent[-c(1:i)]
+    clipped <- cent[1:length(lag)]
+    ACF[i +1] <- (sum(clipped * lag)/n)/z
+}
+
+round(ACF, 6)[1:22] == round(as.vector(acf_ap$acf), 6)
